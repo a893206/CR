bbcf9bbf27df08aa5b1d9026f671975523476cb6
CR <931009686@qq.com>
2020-08-15 09:30:12 +0800

    test branch

--- HelloWorld/src/com/cr/HelloWorld.java
+++ HelloWorld/src/com/cr/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("0814");
         System.out.println("0815");
+        System.out.println("test branch");
     }
 }
